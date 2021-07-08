ddf3e9af55a1fdfe5b39e3d3d88a843dae701695
Abner Cardenas <abner.cp@hotmail.com>
2021-07-08 00:04:03 -0400

    env

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -2,6 +2,8 @@
 const { response } = require('express');
 const { isValidObjectId } = require('mongoose');
 const { Usuario, Turno, Team } = require('../models');
+const usuario = require('../models/usuario');
+const { usuariosDelete } = require('./usuarios');
 
 const coleccionesPermitidas = [
     'usuarios',
@@ -12,51 +14,123 @@ const coleccionesPermitidas = [
 ];
 
 
-const buscarUsuarios = async (termino='', res= response)=> {
+const buscarUsuarios = async (termino = '', res = response) => {
     const esMongoID = isValidObjectId(termino);
-    if( esMongoID){
+    if (esMongoID) {
         const usuario = await Usuario.findById(termino);
-        res.json({
-            results: (usuario) ? [ usuario ] : []
+        return res.json({
+            results: (usuario) ? [usuario] : []
         })
     }
+
+    const regex = new RegExp(termino, 'i');
+    const usuarios = await Usuario.find({
+        $or: [{ nombre: regex }, { correo: regex }],
+        $and: [{ estado: true }]
+    });
+
+    res.json({
+        //total: usuario.length,
+        results: usuarios
+    });
 }
 
 
-const buscar = (req, res= response) => {
+const buscarTurnos = async (termino = '', res = response) => {
+    const esMongoID = isValidObjectId(termino);
+    if (esMongoID) {
+        const turno = await Turno.findById(termino);
+        return res.json({
+            results: (turno) ? [turno] : []
+        })
+    }
+
+    const regex = new RegExp(termino, 'i');
+    const turnos = await Turno.find({ nombre: regex, estado: true });
+
+    res.json({
+        //total: usuario.length,
+        results: turnos
+    });
+}
+
+
+const buscarTeams = async (termino = '', res = response) => {
+    const esMongoID = isValidObjectId(termino);
+    if (esMongoID) {
+        const team = await Team.findById(termino); //incoporar populate con cliente
+        return res.json({
+            results: (team) ? [team]: []
+        })
+    }
+
+    const regex = new RegExp(termino, 'i');
+    const teams = await Team.find({ nombre: regex, estado: true }); //incorporar populate con cliente
+
+    res.json({
+        //total: usuario.length,
+        results: teams
+    });
+
+}
+const buscarClientes = async (termino = '', res = response) => {
+    const esMongoID = isValidObjectId(termino);
+    if (esMongoID) {
+        const team = await Team.findById(termino); //incoporar populate con cliente
+        return res.json({
+            results: (team) ? [team]: []
+        })
+    }
+
+    const regex = new RegExp(termino, 'i');
+    const teams = await Team.find({ nombre: regex, estado: true }); //incorporar populate con cliente
+
+    res.json({
+        //total: usuario.length,
+        results: teams
+    });
+}
+
+
+
+
+const buscar = (req, res = response) => {
 
     const { coleccion, termino } = req.params;
 
-    if( !coleccionesPermitidas.includes( coleccion ) ){
+    if (!coleccionesPermitidas.includes(coleccion)) {
 
-            return res.status(400).json({
-                msg: `las colecciones permitidas son: ${ coleccionesPermitidas }`
-            })
+        return res.status(400).json({
+            msg: `las colecciones permitidas son: ${coleccionesPermitidas}`
+        })
     }
 
     switch (coleccion) {
         case 'usuarios':
             buscarUsuarios(termino, res);
-            
             break;
+
         case 'turnos':
-            
+            buscarTurnos(termino, res);
             break;
+
         case 'teams':
-            
+            buscarTeams(termino, res);
             break;
+
         case 'clientes':
-            
+            buscarClientes(termino, res);
             break;
-    
+
         default:
             res.status(500).json({
                 msg: 'busqueda incompleta!!!'
             })
     }
 
-
 }
+
+
 
 module.exports = {
     buscar
---
+++ routes/clientes.js
@@ -0,0 +1,40 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarJWT,
+     validarCampos,
+      tieneRole,
+       AdminRole} = require('../middlewares');
+
+const { esRoleValido, emailExiste, existeUsuarioById } = require('../helpers/db-validators');
+
+const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, obtenerUsuario } = require('../controllers/usuarios');
+
+
+
+const router = Router();
+
+router.get('/', usuariosGet);
+
+router.get('/:id',[
+    check('id', 'NO es un ID válido').isMongoId(),
+    check('id').custom(existeUsuarioById),
+    validarCampos
+] ,obtenerUsuario);
+
+router.post('/', [
+    check('nombre', 'el nombre no es válido').not().isEmpty(),
+    check('apellido', 'el apellido no es válido').not().isEmpty(),
+    check('password', 'el password debe ser de más de 6 letras').isLength({ min: 6 }),
+    check('correo', 'el correo no es válido').isEmail(),
+    check('correo').custom( emailExiste ),
+    //check('rol', 'ROl no es válido').isIn( [ 'ADMIN_ROLE', 'USER_ROLE' ] ),
+    check('rol').custom( esRoleValido), // esRoleValido recibe el primer valor del custom, osea; ROL
+    validarCampos
+], usuariosPost);
+
+
+
+
+
+module.exports = router;
